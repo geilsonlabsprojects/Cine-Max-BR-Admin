ee19235a1cd3ae603cb8807c6c581f9d6968498c
Geilson Labs Projects <geilsonlabsprojects@gmail.com>
2026-07-10 12:59:29 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+/**
+ * CINE MAX CONFIGURATION
+ * This file is git-ignored and populated during deploy.
+ */
+window.CINE_MAX_CONFIG = {
+    FIREBASE_API_KEY: "",
+    FIREBASE_AUTH_DOMAIN: "",
+    FIREBASE_PROJECT_ID: "",
+    FIREBASE_DATABASE_URL: "",
+    FIREBASE_STORAGE_BUCKET: "",
+    FIREBASE_SENDER_ID: "",
+    FIREBASE_APP_ID: "",
+    TMDB_API_KEY: "",
+    GEMINI_API_KEY: ""
+};
